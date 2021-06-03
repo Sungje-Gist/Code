642c238a38f1f64263b292e373328ea9c429e57d
Sungje-Gist <85298561+Sungje-Gist@users.noreply.github.com>
2021-06-03 23:14:08 +0900

    Add files via upload

---
+++ Python 17강.py
@@ -0,0 +1,13 @@
+# int(문자열을 int형으로 변환함), float(문자열을 float형으로 변환함), str(숫자열을 문자열로 변환함)
+# bool(??) --> Ture of False? 
+# ?? = None, 0, 0.0, 빈 컨테이너
+# 0 and 0.0 --> False. 0이 아닌 상수 --> True.
+# 빈 컨테이너("", b"", [], (), {}) = False.
+# pass키워드: 아무 의미는 없지만 프로그램을 실행하게 해주는 키워드이다. --> 위치를 찾게 해준다.
+# raise NotImplementError 삽입시 구현을 하지 않았다는것을 쉽게 파악할 수 있다.
+
+if True:
+    pass
+else:
+    pass
+
